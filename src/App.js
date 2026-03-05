5a553acda28d90e756e03ae0bce6bc0bc8b6d751
Ajay Joy <ajay.joy@pl.abb.com>
2026-03-05 12:03:59 +0100

    CSS fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import './styles/abb.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import PolicyViewer from './PolicyViewer.js';
-import PolicyList from './pages/PolicyList.jsx';
+// import PolicyList from './pages/PolicyList.jsx';
 
 function App() {
   console.log("App component rendered");
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router basename="/ABB-MobileDistribution">
       <Routes>
-        <Route path="/" element={<PolicyList />} />
+        {/* <Route path="/" element={<PolicyList />} /> */}
         <Route path="/view/:appName/:fileName" element={<PolicyViewer />} />
       </Routes>
     </Router>
